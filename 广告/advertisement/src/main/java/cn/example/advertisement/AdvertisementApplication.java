f6513a2aed643f2f28b9f3740e35c6807da1b438
Whelm000 <ln123.123@outlook.com>
2024-12-25 21:16:19 +0800

    创建广告后端springboot项目，导入所需依赖

---
+++ 广告/advertisement/src/main/java/cn/example/advertisement/AdvertisementApplication.java
@@ -0,0 +1,13 @@
+package cn.example.advertisement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvertisementApplication.class, args);
+    }
+
+}
---
+++ 广告/advertisement/src/test/java/cn/example/advertisement/AdvertisementApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.example.advertisement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
